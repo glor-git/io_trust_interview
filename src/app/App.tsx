9bde7dbad973d0232ae7311e583ceddb4a0398a9
glor-git <sykkkkk412@naver.com>
2025-06-18 12:07:25 +0900

    init

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Route, Routes } from 'react-router-dom'
-
-export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<div className=""></div>}></Route>
-    </Routes>
-  )
-}
--- src/lib/utils/tailwindUtil.ts
+++ src/lib/utils/tailwindUtil.ts
@@ -1,24 +1,6 @@
-import { clsx, type ClassValue } from 'clsx'
+import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
-/**
- * Usage
- * ```tsx
- * import { cn } from '@/lib/utils/tailwindUtil'
- * 
- * return (
- *  <button 
- *      className={cn([
- *          'px-4 py-2 rounded',
- *          'bg-blue-500 text-white',
- *          'hover:bg-blue-600',
- *          isDisabled && 'bg-slate-400 text-slate-600 cursor-not-allowed',
- *      )]}
- *  >
- *  Hello
- * </button>;
- * ```
- */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
